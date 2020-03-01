f8c01ad5588381db87a59677759c8c22666ddf86
kenth <kenth@sfu.ca>
2020-03-01 15:06:56 -0800

    Create basic UI for inspectionReport, implement listView.
    Created the UI for the third user story, implement the listView as well to prepare for violation data.

--- app/src/main/java/videodemos/example/restaurantinspector/UI/InspectionReportActivity.java
+++ app/src/main/java/videodemos/example/restaurantinspector/UI/InspectionReportActivity.java
@@ -3,6 +3,12 @@ package videodemos.example.restaurantinspector.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import videodemos.example.restaurantinspector.R;
 
@@ -12,5 +18,42 @@ public class InspectionReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspection_report);
+
+        populateListView();
+        registerClickCallback():
     }
+
+
+
+    private void populateListView() {
+        String[] myItems = new String[getNumberOfViolations];
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_inspection_report, myItems);
+
+        ListView list = (ListView) findViewById(R.id.violations);
+        list.setAdapter(adapter);
+
+    }
+    private void registerClickCallback() {
+        ListView list = (ListView) findViewById(R.id.violations);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id){
+                TextView textView = (TextView) viewClicked;
+                String message = "a";//the detailed description of the violation
+                Toast.makeText(InspectionReportActivity.this, message, Toast.LENGTH_LONG);
+            }
+
+
+
+
+
+        });
+    }
+
+
+    public Inspection getInspection(){
+
+    }
+
 }
