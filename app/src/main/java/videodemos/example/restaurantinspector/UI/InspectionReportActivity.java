a2aab2aa6bd9403bc5321929b3e11b35e9a40708
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2020-03-05 19:18:47 -0800

    [Awshaf]: Implement natureViolation HashMap

--- app/src/main/java/videodemos/example/restaurantinspector/UI/InspectionReportActivity.java
+++ app/src/main/java/videodemos/example/restaurantinspector/UI/InspectionReportActivity.java
@@ -192,81 +192,81 @@ public class InspectionReportActivity extends AppCompatActivity {
 
     //    private static void populateShortViolation(){
 //        //TODO: Add boolean as second field based on criticallity
-//        shortViolation.put(101, "Plans/construction/alterations not up to standard."); // miscellaneous_icon
-//        shortViolation.put(102,"Operation of unapproved food premises. ");        // miscellaneous_icon
-//        shortViolation.put(103,"No valid permit for this restaurant.");           // miscellaneous_icon
-//        shortViolation.put(104,"Permit not seen easily.");                        // miscellaneous_icon
-//        shortViolation.put(201, "Food contaminated or unfit for consumption.");   //unfit_consumption_icon
-//        shortViolation.put(202,"Food not processed in manner for safe eating.");  // miscellaneous_icon
-//        shortViolation.put(203,"Food not cooled properly.");                      //thaw_hazard_icon
-//        shortViolation.put(204,"Food not cooked or heated for safe eating.");     //unfit_consumption_icon
-//        shortViolation.put(205,"Cold food not stored properly.");                 // miscellaneous_icon
-//        shortViolation.put(206,"Hot food not stored properly.);                   // miscellaneous_icon
-//        shortViolation.put(208,"Food obtained from unapproved sources.");         // miscellaneous_icon
-//        shortViolation.put(209,"Food not protected from contamination.");         // contamination_icon
-//        shortViolation.put(210,"Food not thawed properly.");                      //thaw_hazard_icon
-//        shortViolation.put(211,"Frozen food not stored properly.");               // miscellaneous_icon
-//        shortViolation.put(212,"Food handling procedures not provided.");         // miscellaneous_icon
-//        shortViolation.put(301,"Equipment/utensils/food not sanitary.");          //sanitory_condition_icon
-//        shortViolation.put(302,"Equipment/utensils/food not properly washed/sanitized."); //sanitory_condition_icon
-//        shortViolation.put(303,"Equipment/facilities/Water for sanitation not proper.");  sanitory_condition_icon
-//        shortViolation.put(304,"Location contains pests.");                       //pests_icon
-//        shortViolation.put(305,"Conditions observed may cultivate pests.");       //pests_icon
-//        shortViolation.put(306,"Food premise not sanitary.");                     //sanitory_condition_icon
-//        shortViolation.put(307,"Equipment/utensils/food contact surfaces bad design/material");   //working_order
-//        shortViolation.put(308,"Equipment/utensils/food contact surfaces not working.");          //working_order
-//        shortViolation.put(309,"Chemical cleansers stored or labelled improperly.");              // miscellaneous_icon
-//        shortViolation.put(310,"Single use items used more than once.);                           // miscellaneous_icon
-//        shortViolation.put(311,"Premise not maintained according to plan.");                      // miscellaneous_icon
-//        shortViolation.put(312,"Items unrelated to food business on premise.");                   // miscellaneous_icon
-//        shortViolation.put(313, "Live animals on premise.");                                      // miscellaneous_icon
-//        shortViolation.put(314,"Approved sanitation procedures not provided.");                   // sanitory_condition_icon
-//        shortViolation.put(315,"Inaccurate thermometers.");                                       // working_order
-//        shortViolation.put(401,"Proper handwashing station not present.");                        // sanitory_condition_icon
-//        shortViolation.put(402,"Hands not washed adequately or frequently enough.");              // sanitory_condition_icon
-//        shortViolation.put(403,"Employees lack hygiene.");                                        // unhygienic_icon
-//        shortViolation.put(404,"Employees smoking in not proper areas.");                         // miscellaneous_icon
-//        shortViolation.put(501,"FOODSAFE level 1 or Equivalent not present.");                    // miscellaneous_icon
-//        shortViolation.put(502,"In Operator's absence, nobody has FOODSAFE level 1 or Equivalent."); // miscellaneous_icon
+//        shortViolation.put(101, "Plans/construction/alterations not up to standard.");
+//        shortViolation.put(102,"Operation of unapproved food premises. ");
+//        shortViolation.put(103,"No valid permit for this restaurant.");
+//        shortViolation.put(104,"Permit not seen easily.");
+//        shortViolation.put(201, "Food contaminated or unfit for consumption.");
+//        shortViolation.put(202,"Food not processed in manner for safe eating.");
+//        shortViolation.put(203,"Food not cooled properly.");
+//        shortViolation.put(204,"Food not cooked or heated for safe eating.");
+//        shortViolation.put(205,"Cold food not stored properly.");
+//        shortViolation.put(206,"Hot food not stored properly.);
+//        shortViolation.put(208,"Food obtained from unapproved sources.");         //
+//        shortViolation.put(209,"Food not protected from contamination.");         //
+//        shortViolation.put(210,"Food not thawed properly.");                      //
+//        shortViolation.put(211,"Frozen food not stored properly.");               //
+//        shortViolation.put(212,"Food handling procedures not provided.");         //
+//        shortViolation.put(301,"Equipment/utensils/food not sanitary.");          //
+//        shortViolation.put(302,"Equipment/utensils/food not properly washed/sanitized."); //
+//        shortViolation.put(303,"Equipment/facilities/Water for sanitation not proper.");
+//        shortViolation.put(304,"Location contains pests.");                       //
+//        shortViolation.put(305,"Conditions observed may cultivate pests.");       //
+//        shortViolation.put(306,"Food premise not sanitary.");                     //
+//        shortViolation.put(307,"Equipment/utensils/food contact surfaces bad design/material");   //
+//        shortViolation.put(308,"Equipment/utensils/food contact surfaces not working.");          //
+//        shortViolation.put(309,"Chemical cleansers stored or labelled improperly.");              //
+//        shortViolation.put(310,"Single use items used more than once.);                           //
+//        shortViolation.put(311,"Premise not maintained according to plan.");                      //
+//        shortViolation.put(312,"Items unrelated to food business on premise.");                   //
+//        shortViolation.put(313, "Live animals on premise.");                                      //
+//        shortViolation.put(314,"Approved sanitation procedures not provided.");                   //
+//        shortViolation.put(315,"Inaccurate thermometers.");                                       //
+//        shortViolation.put(401,"Proper handwashing station not present.");                        //
+//        shortViolation.put(402,"Hands not washed adequately or frequently enough.");              //
+//        shortViolation.put(403,"Employees lack hygiene.");                                        //
+//        shortViolation.put(404,"Employees smoking in not proper areas.");                         //
+//        shortViolation.put(501,"FOODSAFE level 1 or Equivalent not present.");                    //
+//        shortViolation.put(502,"In Operator's absence, nobody has FOODSAFE level 1 or Equivalent."); //
     //  }
 //public static HashMap<Integer, String> natureViolation = new HashMap<Integer, String>();
     //    private static void populateShortViolation(){
 //        //TODO: Add boolean as second field based on criticallity
 //        natureViolation.put(101, "miscellaneous_icon");
-//        natureViolation.put(102,"");
-//        natureViolation.put(103,"");
-//        natureViolation.put(104,"");
-//        natureViolation.put(201, "");
-//        natureViolation.put(202,"");
-//        natureViolation.put(203,"");
-//        natureViolation.put(204,"");
-//        natureViolation.put(205,"");
-//        natureViolation.put(206,");
-//        natureViolation.put(208,"");
-//        natureViolation.put(209,"");
-//        natureViolation.put(210,"");
-//        natureViolation.put(211,"");
-//        natureViolation.put(212,"");
-//        natureViolation.put(301,"");
-//        natureViolation.put(302,"");
-//        natureViolation.put(303,"");
-//        natureViolation.put(304,"");
-//        natureViolation.put(305,"");
-//        natureViolation.put(306,"");
-//        natureViolation.put(307,"");
-//        natureViolation.put(308,"");
-//        natureViolation.put(309,"");
-//        natureViolation.put(310,"");
-//        natureViolation.put(311,"");
-//        natureViolation.put(312,"");
-//        natureViolation.put(313,"");
-//        natureViolation.put(314,"");
-//        natureViolation.put(315,"");
-//        natureViolation.put(401,"");
-//        natureViolation.put(402,"");
-//        natureViolation.put(403,"");
-//        natureViolation.put(404,"");
-//        natureViolation.put(501,"");
-//        natureViolation.put(502,"");
+//        natureViolation.put(102,"miscellaneous_icon");
+//        natureViolation.put(103,"miscellaneous_icon");
+//        natureViolation.put(104,"miscellaneous_icon");
+//        natureViolation.put(201, "unfit_consumption_icon");
+//        natureViolation.put(202,"miscellaneous_icon");
+//        natureViolation.put(203,"thaw_hazard_icon");
+//        natureViolation.put(204,"unfit_consumption_icon");
+//        natureViolation.put(205,"miscellaneous_icon");
+//        natureViolation.put(206,"miscellaneous_icon");
+//        natureViolation.put(208,"miscellaneous_icon");
+//        natureViolation.put(209,"contamination_icon");
+//        natureViolation.put(210,"thaw_hazard_icon");
+//        natureViolation.put(211,"miscellaneous_icon");
+//        natureViolation.put(212,"miscellaneous_icon");
+//        natureViolation.put(301,"sanitory_condition_icon");
+//        natureViolation.put(302,"sanitory_condition_icon");
+//        natureViolation.put(303,"sanitory_condition_icon");
+//        natureViolation.put(304,"pests_icon");
+//        natureViolation.put(305,"pests_icon");
+//        natureViolation.put(306,"sanitory_condition_icon");
+//        natureViolation.put(307,"working_order");
+//        natureViolation.put(308,"working_order");
+//        natureViolation.put(309,"miscellaneous_icon");
+//        natureViolation.put(310,"miscellaneous_icon");
+//        natureViolation.put(311,"miscellaneous_icon");
+//        natureViolation.put(312,"miscellaneous_icon");
+//        natureViolation.put(313,"miscellaneous_icon");
+//        natureViolation.put(314,"sanitory_condition_icon");
+//        natureViolation.put(315,"working_order");
+//        natureViolation.put(401,"sanitory_condition_icon");
+//        natureViolation.put(402,"sanitory_condition_icon");
+//        natureViolation.put(403,"unhygienic_icon");
+//        natureViolation.put(404,"miscellaneous_icon");
+//        natureViolation.put(501,"miscellaneous_icon");
+//        natureViolation.put(502,"miscellaneous_icon");
     //  }
 }
