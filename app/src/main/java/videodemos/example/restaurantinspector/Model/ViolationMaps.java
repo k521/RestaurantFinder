86e3d41f4f71742ee3874416ecc80dc57bb3c6a0
Cesar Moreno <cmoreno@sfu.ca>
2020-03-09 20:13:16 -0700

    Refactor strings in ViolationMaps.violationCodes to be in strings file.

--- app/src/main/java/videodemos/example/restaurantinspector/Model/ViolationMaps.java
+++ app/src/main/java/videodemos/example/restaurantinspector/Model/ViolationMaps.java
@@ -44,59 +44,59 @@ public class ViolationMaps {
     public static HashMap<Integer, String> shortViolation = new HashMap<Integer, String>();
 
 
-    private static void populateMonths() {
-        months.put(1, "January");
-        months.put(2, "February");
-        months.put(3, "March");
-        months.put(4, "April");
-        months.put(5, "May");
-        months.put(6, "June");
-        months.put(7, "July");
-        months.put(8, "August");
-        months.put(9, "September");
-        months.put(10, "October");
-        months.put(11, "November");
-        months.put(12, "December");
+    private void populateMonths() {
+        months.put(1, context.getResources().getString(R.string.january));
+        months.put(2, context.getResources().getString(R.string.february));
+        months.put(3, context.getResources().getString(R.string.march));
+        months.put(4, context.getResources().getString(R.string.april));
+        months.put(5, context.getResources().getString(R.string.may));
+        months.put(6, context.getResources().getString(R.string.june));
+        months.put(7, context.getResources().getString(R.string.july));
+        months.put(8, context.getResources().getString(R.string.august));
+        months.put(9, context.getResources().getString(R.string.september));
+        months.put(10, context.getResources().getString(R.string.october));
+        months.put(11, context.getResources().getString(R.string.november));
+        months.put(12, context.getResources().getString(R.string.december));
 
     }
 
     private void populateViolations() {
-        violationCodes.put(101, "Not Critical,Plans/construction/alterations not in accordance with the Regulation [s. 3; s. 4],Not Repeat");
-        violationCodes.put(102, "Not Critical,Operation of an unapproved food premises [s. 6(1)],Not Repeat");
-        violationCodes.put(103, "Not Critical,Failure to hold a valid permit while operating a food service establishment [s. 8(1)],Not Repeat");
-        violationCodes.put(104, "Not Critical,Permit not posted in a conspicuous location [s. 8(7)],Not Repeat");
-        violationCodes.put(201, "Critical,Food contaminated or unfit for human consumption [s. 13],Not Repeat");
-        violationCodes.put(202, "Critical,Food not processed in a manner that makes it safe to eat [s. 14(1)],Not Repeat");
-        violationCodes.put(203, "Critical,Food not cooled in an acceptable manner [s. 12(a)],Not Repeat");
-        violationCodes.put(204, "Critical,Food not cooked or reheated in a manner that makes it safe to eat [s. 14(1)],Not Repeat");
-        violationCodes.put(205, "Critical,Cold potentially hazardous food stored/displayed above 4 °C. [s. 14(2)],Not Repeat");
-        violationCodes.put(206, "Critical,Hot potentially hazardous food stored/displayed below 60 °C. [s. 14(2)],Not Repeat");
-        violationCodes.put(208, "Not Critical,Foods obtained from unapproved sources [s. 11],Not Repeat");
-        violationCodes.put(209, "Not Critical,Food not protected from contamination [s. 12(a)],Not Repeat");
-        violationCodes.put(210, "Not Critical,Food not thawed in an acceptable manner [s. 14(2)],Not Repeat");
-        violationCodes.put(211, "Not Critical,Frozen potentially hazardous food stored/displayed above -18 °C. [s. 14(3)],Not Repeat");
-        violationCodes.put(212, "Not Critical,Operator has not provided acceptable written food handling procedures [s. 23],Not Repeat");
-        violationCodes.put(301, "Critical,Equipment/utensils/food contact surfaces not maintained in sanitary condition [s. 17(1)],Not Repeat");
-        violationCodes.put(302, "Critical,Equipment/utensils/food contact surfaces not properly washed and sanitized [s. 17(2)],Not Repeat");
-        violationCodes.put(303, "Critical,Equipment/facilities/hot & cold water for sanitary maintenance not adequate [s. 17(3); s. 4(1)(f)],Not Repeat");
-        violationCodes.put(304, "Not Critical,Premises not free of pests [s. 26(a)],Not Repeat");
-        violationCodes.put(305, "Not Critical,Conditions observed that may allow entrance/harbouring/breeding of pests [s. 26(b)(c)],Not Repeat");
-        violationCodes.put(306, "Not Critical,Food premises not maintained in a sanitary condition [s. 17(1)],Not Repeat");
-        violationCodes.put(307, "Not Critical,Equipment/utensils/food contact surfaces are not of suitable design/material [s. 16; s. 19],Not Repeat");
-        violationCodes.put(308, "Not Critical,Equipment/utensils/food contact surfaces are not in good working order [s. 16(b)],Not Repeat");
-        violationCodes.put(309, "Not Critical,Chemicals cleansers & similar agents stored or labeled improperly [s. 27],Not Repeat");
-        violationCodes.put(310, "Not Critical,Single use containers & utensils are used more than once [s. 20],Not Repeat");
-        violationCodes.put(311, "Not Critical,Premises not maintained as per approved plans [s. 6(1)(b)],Not Repeat");
-        violationCodes.put(312, "Not Critical,Items not required for food premises operation being stored on the premises [s. 18],Not Repeat");
-        violationCodes.put(313, "Not Critical,Live animal on the premises excluding guide animal in approved areas [s. 25(1)],Not Repeat");
-        violationCodes.put(314, "Not Critical,Operator has not provided acceptable written sanitation procedures [s. 24],Not Repeat");
-        violationCodes.put(315, "Not Critical,Refrigeration units and hot holding equipment lack accurate thermometers [s. 19(2)],Not Repeat");
-        violationCodes.put(401, "Critical,Adequate handwashing stations not available for employees [s. 21(4)],Not Repeat");
-        violationCodes.put(402, "Critical,Employee does not wash hands properly or at adequate frequency [s. 21(3)],Not Repeat");
-        violationCodes.put(403, "Not Critical,Employee lacks good personal hygiene clean clothing and hair control [s. 21(1)],Not Repeat");
-        violationCodes.put(404, "Not Critical,Employee smoking in food preparation/processing/storage areas [s. 21(2)],Not Repeat");
-        violationCodes.put(501, "Not Critical,Operator does not have FOODSAFE Level 1 or Equivalent [s. 10(1)],Not Repeat");
-        violationCodes.put(502, "Not Critical,In operator’s absence no staff on duty has FOODSAFE Level 1 or equivalent [s. 10(2)],Not Repeat");
+        violationCodes.put(101, context.getResources().getString(R.string.violationDescription101));
+        violationCodes.put(102, context.getResources().getString(R.string.violationDescription102));
+        violationCodes.put(103, context.getResources().getString(R.string.violationDescription103));
+        violationCodes.put(104, context.getResources().getString(R.string.violationDescription104));
+        violationCodes.put(201, context.getResources().getString(R.string.violationDescription201));
+        violationCodes.put(202, context.getResources().getString(R.string.violationDescription202));
+        violationCodes.put(203, context.getResources().getString(R.string.violationDescription203));
+        violationCodes.put(204, context.getResources().getString(R.string.violationDescription204));
+        violationCodes.put(205, context.getResources().getString(R.string.violationDescription205));
+        violationCodes.put(206, context.getResources().getString(R.string.violationDescription206));
+        violationCodes.put(208, context.getResources().getString(R.string.violationDescription208));
+        violationCodes.put(209, context.getResources().getString(R.string.violationDescription209));
+        violationCodes.put(210, context.getResources().getString(R.string.violationDescription210));
+        violationCodes.put(211, context.getResources().getString(R.string.violationDescription211));
+        violationCodes.put(212, context.getResources().getString(R.string.violationDescription212));
+        violationCodes.put(301, context.getResources().getString(R.string.violationDescription301));
+        violationCodes.put(302, context.getResources().getString(R.string.violationDescription302));
+        violationCodes.put(303, context.getResources().getString(R.string.violationDescription303));
+        violationCodes.put(304, context.getResources().getString(R.string.violationDescription304));
+        violationCodes.put(305, context.getResources().getString(R.string.violationDescription305));
+        violationCodes.put(306, context.getResources().getString(R.string.violationDescription306));
+        violationCodes.put(307, context.getResources().getString(R.string.violationDescription307));
+        violationCodes.put(308, context.getResources().getString(R.string.violationDescription308));
+        violationCodes.put(309, context.getResources().getString(R.string.violationDescription309));
+        violationCodes.put(310, context.getResources().getString(R.string.violationDescription310));
+        violationCodes.put(311, context.getResources().getString(R.string.violationDescription311));
+        violationCodes.put(312, context.getResources().getString(R.string.violationDescription312));
+        violationCodes.put(313, context.getResources().getString(R.string.violationDescription313));
+        violationCodes.put(314, context.getResources().getString(R.string.violationDescription314));
+        violationCodes.put(315, context.getResources().getString(R.string.violationDescription315));
+        violationCodes.put(401, context.getResources().getString(R.string.violationDescription401));
+        violationCodes.put(402, context.getResources().getString(R.string.violationDescription402));
+        violationCodes.put(403, context.getResources().getString(R.string.violationDescription403));
+        violationCodes.put(404, context.getResources().getString(R.string.violationDescription404));
+        violationCodes.put(501, context.getResources().getString(R.string.violationDescription501));
+        violationCodes.put(502, context.getResources().getString(R.string.violationDescription502));
     }
 
     private void populateSeverity() {
