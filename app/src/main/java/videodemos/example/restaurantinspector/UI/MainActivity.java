279a04a580dd71b35fcc0d5255e3c0345931c049
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2020-03-17 09:33:18 -0700

    [Awshaf]: Implement validation for GooglePlayServices

--- app/src/main/java/videodemos/example/restaurantinspector/UI/MainActivity.java
+++ app/src/main/java/videodemos/example/restaurantinspector/UI/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import videodemos.example.restaurantinspector.Model.Restaurant;
 import videodemos.example.restaurantinspector.Model.RestaurantManager;
@@ -35,22 +39,13 @@ public class MainActivity extends AppCompatActivity implements RestaurantsAdapte
         setupRestaurantManager();
         setUpRestaurantsRecylerView();
 
-        setupMapButton();
+
+        if( isServicesOK() ){ //checks for functioning Google Services
+            setupMapButton();
+        }
 
     }
 
-    private void setupMapButton() {
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent (MainActivity.this, MapsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-    }
 
     private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.restaurant_list_toolbar);
@@ -88,4 +83,45 @@ public class MainActivity extends AppCompatActivity implements RestaurantsAdapte
             Intent intent = RestaurantReportActivity.makeIntent(this, position);
             startActivity(intent);
     }
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void setupMapButton() {
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent (MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
 }
